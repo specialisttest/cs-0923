fc4db892df308c11bc883613c2cb9e7dda834ec0
Sergey Shuykov <sshuykov@specialist.ru>
2024-09-25 21:30:10 +0300

    day 3

---
+++ Iterations/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace Iterations
+{
+    internal class Program
+    {
+        public static IEnumerable<string> GetCourses()
+        {
+            //return new List<string> { "Язык C# 12.0",
+            //    "Клиент-серверная разработка под .Net На C#", "Pattern OOP"};
+            yield return "Язык C# 12.0";
+            yield return "Pattern OOP";
+            //yield break; // заканчивает создание генератора
+            yield return "Клиент-серверная разработка под .Net На C#";
+        }
+        
+        static void Main(string[] args)
+        {
+            //int i = 10;
+            ////i = i + 1; 
+            ////i += 1;
+            //int x = i++;
+
+            //Console.WriteLine(i);
+            //Console.WriteLine(x);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 8) break;
+                if (i == 4) continue;
+                Console.WriteLine(i);
+            }
+
+            // 1 2 3 .. 10
+            // 2 4 6 .. 20
+            // ...
+            // 10 20 30 .. 100
+            for (int i = 1;i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (j == 5) goto metka;
+                    Console.Write("{0, -5}", i * j);
+                }
+                Console.WriteLine();
+            }
+        metka:
+            Console.WriteLine("\ncontinue");
+
+            int a = 2000;
+            while (a < 1000)
+            { 
+                Console.WriteLine(a);
+                a *= 2;
+            }
+
+            a = 2;
+            do 
+            {
+                Console.WriteLine(a);
+                //a *= 2;
+            } while ( (a *= 2) < 1000);
+
+            /*
+             * Вывести числа Фибоначчи < 1000
+             * 0 1 1 2 3 5 8 13 21 ...
+             * 
+             */
+            int k1 = 0, k2 = 1, k3;
+            Console.Write($"{k1} {k2} ");
+            while ((k3 = k1 + k2) < 1000)
+            {
+                Console.Write("{0} ", k3);
+                k1 = k2;
+                k2 = k3;
+            }
+
+            Console.WriteLine();
+
+            ICollection<string> courses = new List<string> { "Язык C# 12.0",
+                "Клиент-серверная разработка под .Net На C#", "Pattern OOP"};
+
+            //for (int i = 0; i < courses.Count; i++)
+            //    Console.WriteLine(courses[i]); // не работает для ICollection
+
+            // если источник реализует IEnumerable или содержит метод IEnumerator GetEnumerator()
+            //foreach (var course in courses)
+            //    Console.WriteLine(course);
+
+            //IEnumerator<string> en = courses.GetEnumerator();
+            //while (en.MoveNext()) 
+            //{
+            //    string course = en.Current;
+            //    Console.WriteLine( course );
+            //}
+
+            foreach (var course in GetCourses())
+                Console.WriteLine(course);
+
+
+            //int n = int.Parse( Console.ReadLine() );
+
+            /*
+             * 1. Пользователь вводит целое число от 0 до 9 - количество ворон на ветке
+             *    На ветке 5 ворон
+             * 2. Для любого числа
+             * 
+             **/
+
+
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Operators
+{
+    internal class Program
+    {
+        static string num(int a)
+        {
+            switch (a)
+            {
+                case -1:
+                case 1 : return "один";
+                case 2 : return "два";
+                case 3 : return "три";
+                default: return "много";
+            }
+        }
+
+        static string num2(int a) => // switch expression
+            a switch
+            {
+                -1 or 1 => "один",
+                2 => "два",
+                3 => "три",
+                _ => "много"
+            };
+        
+
+
+        static void Main(string[] args)
+        {
+            int n = -5;
+            if (n > 0)
+            {
+                Console.WriteLine("n больше нуля");
+                Console.WriteLine("n > 0");
+            }
+            else 
+                if (n > -100)
+                    Console.WriteLine("n НЕ больше нуля, но больше -100");
+                else
+                    Console.WriteLine("n <= 0");
+
+            int a = 0;
+            /*string s;
+            if (a == 0)
+                s = "ноль";
+            else
+                s = "не ноль";*/
+
+            string s = (a == 0) ? "ноль" : "не ноль";
+
+            Console.WriteLine(s);
+
+            string name = null; // "Sergey";
+
+            //if (name == null) name = "DEFAULT NAME";
+            name ??= "DEFAULT NAME";
+
+            Console.WriteLine(  ( name != null ? name : "<пустое имя>" ).ToUpper());
+
+            Console.WriteLine((name ?? "<пустое имя>").ToUpper());
+            Console.WriteLine( name?.ToUpper() );
+
+            a = -1;
+            switch (a)
+            {
+                case -1:
+                case 1: 
+                    Console.WriteLine("один");
+                    break; // return
+                case 1+1: 
+                    Console.WriteLine("два");
+                    break;
+                case 3:
+                    Console.WriteLine("три");
+                    break;
+                default:
+                    Console.WriteLine("много");
+                    break;
+            }
+
+            Console.WriteLine(num(2));
+            Console.WriteLine(num2(3));
+
+            string r = a switch // switch expression
+            {
+                -1 or 1 => "один",
+                2 => "два",
+                3 => "три",
+                _ => "много"
+            };
+
+            Console.WriteLine( r );
+
+            //System.Object o;
+            object o = "String";
+            o = 5; // boxing
+            int k = (int)o; // unboxing
+
+
+            switch (o) // pattern matching
+            {
+                case int i when i >=0 && i <=100:
+                    Console.WriteLine($"int o in [0, 100] {i}"); break;
+                case int: Console.WriteLine("int o"); break;
+                case string: Console.WriteLine("string o"); break;
+                case double: Console.WriteLine("double o"); break;
+                default: Console.WriteLine("???? o"); break;
+            }
+
+
+
+        }
+    }
+}
