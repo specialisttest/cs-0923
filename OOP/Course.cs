d67ef25dda981ce5b9aa105b8f1edaea18b13027
Sergey Shuykov <sshuykov@specialist.ru>
2024-09-27 21:28:04 +0300

    day 5

---
+++ OOP/Course.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Course
+    {
+        /*private string title = "Новый курс";
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }*/
+
+        // auto property
+        //public string Title { get; set; }
+        public string Title { get; init; } = "Новый курс";
+
+        public string Description { get; init; }
+
+        private int length;
+        public int Length
+        {
+            get { return length; }
+            /*private*/
+            set
+            {
+                if (value >= 8 && value <= 48)
+                    this.length = value;
+                else
+                    throw new ArgumentException("Course Length out of [8..48]");
+            }
+        }
+
+        static int counter = 0;
+
+        // вызывается автоматически CLR при первом обращении к классу
+        // используется для сложной инициализации статических полей
+        static Course()
+        {
+        
+        }
+
+        public static void ShowCoursesCounter()
+        {
+            Console.WriteLine($"Total Courses: {counter}");
+        }
+
+
+        public Course()
+            : this("New course", 16) // вызов другого конструктора этого же класса
+        {
+
+        }
+
+        public Course(string Title, int Length = 8) 
+        { 
+            this.Title = Title;
+            this.Length = Length; // call set
+            counter++;
+        }
+
+        public void Show()
+        {
+            // this - ссылка на объект, для которого вызван этот метод
+            Console.WriteLine($"{this.Title} : {Length}");
+        }
+
+        ~Course() 
+        { 
+            Console.WriteLine("~Course()");
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Math;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course.ShowCoursesCounter();
+            
+            Course c1 = new Course("C# 12", 40) { Description = "Введение в язык c# 12"};
+            //c1.Title = "C# 12";
+            //c1.Length = 40;
+            c1.Length = 48; // call set
+            //c1.Title = "Язык C# 12";
+            Console.WriteLine(c1.Length); // call get
+
+            //c1.SetLength(48);
+            //Console.WriteLine(c1.GetLength());
+
+            c1.Show(); // this == c1
+
+            Course c2 = new Course("Patterns OOP", 24);
+            //c2.Title = "Patterns OOP";
+            //c2.Length = 24;
+
+            c2.Show(); // c2 == this
+
+            Course c3 = new Course();
+            c3.Show();
+
+            // y = log(sin(M_PI * x))
+            double x = 5;
+            //double y = Math.Log(Math.Sin(Math.PI * x));
+            double y = Log(Sin(PI * x));
+
+            //c3 = null;
+            //GC.Collect();
+
+            Course.ShowCoursesCounter();
+        }
+    }
+}
